a2861cabee011cc7f7ae0f8cab6dc01ff07e43e6
Grady <43865922+OrcaCrafter@users.noreply.github.com>
2025-12-16 10:43:30 -0700

    Fixed inconsistency in project name

--- src/main/java/orca/shearable_players/ShearablePlayers.java
+++ src/main/java/orca/shearable_players/ShearablePlayers.java
@@ -2,14 +2,16 @@ package orca.shearable_players;
 
 import net.fabricmc.api.ModInitializer;
 
+import orca.shearable_players.mixin.ShearsMixin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ShearablePlayer implements ModInitializer {
-	public static final String MOD_ID = "shearableplayer";
+public class ShearablePlayers implements ModInitializer {
+	public static final String MOD_ID = "shearable_players";
 	
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {}
+	
 }
